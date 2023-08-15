337d95914e4b72c255a930f1202f12e34b37122a
Masumraihan <mdmasumraihan1@gamil.com>
2023-08-16 02:23:03 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Accordion from './Components/Accordion'
+import image from "./assets/Group-12682.png"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Accordion />
-    </>
+    <main className='min-h-screen w-full py-20 lg:py-0 flex flex-col items-center justify-center'>
+      <div className='container w-full h-full grid lg:grid-cols-2 gap-10 items-center px-6 lg:px-14'>
+        <div className='space-y-7 order-2 lg:order-1'>
+          <div className='bg-purple-100 p-2 rounded text-purple-500 w-fit'>
+            <p>We Are Leading Software Company</p>
+          </div>
+          <h1 className='text-4xl font-semibold'>We Provide <span className='bg-gradient-to-r from-purple-700 via-purple-600 to-red-500 text-transparent bg-clip-text'>Best Solutions</span> </h1>
+          <p className='text-lg'> Our web design & development company leverages large knowledge base to deliver best-in-domain solutions that meet needs, business and budget expectations of our customers. Our experts offer advanced technology solutions that add real-value to their business. It is simple— we understand that our success is measured by the success of our esteemed clients.</p>
+          <Accordion />
+        </div>
+        <div className='w-full order-1 lg:order-2'>
+          <img className='w-full' src={image} alt="" />
+        </div>
+      </div>
+    </main>
   )
 }
 
--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -1,41 +1,78 @@
-import { Disclosure } from '@headlessui/react'
+import { Disclosure, Transition } from '@headlessui/react'
 import { ChevronUpIcon } from '@heroicons/react/20/solid'
 
 const Accordion = () => {
     return (
-        <div className="w-full px-4 pt-16">
-            <div className="mx-auto w-full max-w-md rounded-2xl bg-white p-2">
+        <div className="w-full">
+            <div className="w-full rounded-2xl bg-white py-2">
                 <Disclosure>
                     {({ open }) => (
-                        <>
-                            <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                                <span>What is your refund policy?</span>
+                        <div className={`${open && "bg-purple-50 "} rounded-lg`}>
+                            <Disclosure.Button className={`flex w-full justify-between rounded-lg px-4 py-2 text-left text-lg font-semibold ${open && 'text-purple-600'} focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75`}>
+                                <span>Create And Save Your Nates With Our Software </span>
                                 <ChevronUpIcon
-                                    className={`${open ? 'rotate-180 transform' : ''
-                                        } h-5 w-5 text-purple-500`}
+                                    className={`${open ? 'rotate-180 transition-transform duration-300 transform' : ''
+                                        } h-5 w-5 transition-transform duration-300 text-purple-500`}
                                 />
                             </Disclosure.Button>
-                            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                                If you re unhappy with your purchase for any reason, email us
-                                within 90 days and we ll refund you in full, no questions asked.
-                            </Disclosure.Panel>
-                        </>
+                            <Transition enter="transition duration-300 ease-out"
+                                enterFrom="transform scale-95 opacity-0"
+                                enterTo="transform scale-100 opacity-100"
+                                leave="transition duration-75 ease-out"
+                                leaveFrom="transform scale-100 opacity-100"
+                                leaveTo="transform scale-95 opacity-0">
+
+                                <Disclosure.Panel className="px-4 pt-4 pb-2">
+                                    You can customize and Create Your task With our software
+                                </Disclosure.Panel>
+                            </Transition>
+                        </div>
                     )}
                 </Disclosure>
                 <Disclosure as="div" className="mt-2">
                     {({ open }) => (
-                        <>
-                            <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                                <span>Do you offer technical support?</span>
+                        <div className={`${open && "bg-purple-50 "} rounded-lg`}>
+                            <Disclosure.Button className={`flex w-full justify-between rounded-lg px-4 py-2 text-left text-lg font-semibold ${open && 'text-purple-600'} focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75`}>
+                                <span>Best In Designing</span>
                                 <ChevronUpIcon
-                                    className={`${open ? 'rotate-180 transform' : ''
-                                        } h-5 w-5 text-purple-500`}
+                                    className={`${open ? 'rotate-180 transform transition-transform duration-300' : ''
+                                        } h-5 w-5 text-purple-500 transition-transform duration-300`}
                                 />
                             </Disclosure.Button>
-                            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                                No.
-                            </Disclosure.Panel>
-                        </>
+                            <Transition enter="transition duration-100 ease-out"
+                                enterFrom="transform scale-95 opacity-0"
+                                enterTo="transform scale-100 opacity-100"
+                                leave="transition duration-75 ease-out"
+                                leaveFrom="transform scale-100 opacity-100"
+                                leaveTo="transform scale-95 opacity-0">
+                                <Disclosure.Panel className="px-4 pt-4 pb-2">
+                                    Create design according to Your need.
+                                </Disclosure.Panel>
+                            </Transition>
+                        </div>
+                    )}
+                </Disclosure>
+                <Disclosure as="div" className="mt-2">
+                    {({ open }) => (
+                        <div className={`${open && "bg-purple-50 "} rounded-lg`}>
+                            <Disclosure.Button className={`flex w-full justify-between rounded-lg px-4 py-2 text-left text-lg font-semibold ${open && 'text-purple-600'} focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75`}>
+                                <span>Protect Your Site With Hosting And SSL</span>
+                                <ChevronUpIcon
+                                    className={`${open ? 'rotate-180 transform transition-transform duration-300' : ''
+                                        } h-5 w-5 text-purple-500 transition-transform duration-300`}
+                                />
+                            </Disclosure.Button>
+                            <Transition enter="transition duration-100 ease-out"
+                                enterFrom="transform scale-95 opacity-0"
+                                enterTo="transform scale-100 opacity-100"
+                                leave="transition duration-75 ease-out"
+                                leaveFrom="transform scale-100 opacity-100"
+                                leaveTo="transform scale-95 opacity-0">
+                                <Disclosure.Panel className="px-4 pt-4 pb-2">
+                                    We are also provide best domain and hosting with SSL
+                                </Disclosure.Panel>
+                            </Transition>
+                        </div>
                     )}
                 </Disclosure>
             </div>
